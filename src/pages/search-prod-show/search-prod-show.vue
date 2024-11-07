ae76e8b6f5663f4df4001d04f4f174cf4038a14b
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2024-11-07 11:33:31 +0800

    搜索唤起与搜索结果页面 search-page.vue + search-prod-show.vue prefix

--- src/pages/search-prod-show/search-prod-show.vue
+++ src/pages/search-prod-show/search-prod-show.vue
@@ -1,37 +1,29 @@
 <template>
   <!--搜索结果页面-->
   <view class="container">
-    <!-- 搜索框 -->
+
+    <!-- 面板区域 -->
     <view class="fixed-box">
+
+      <!-- 搜索框 + 按钮 -->
       <view class="search-bar">
+
         <view class="search-box">
-          <input
-            :value="prodName"
-            class="sear-input"
-            confirm-type="search"
-            placeholder="输入关键字搜索"
-            @confirm="toSearchConfirm"
-            @input="getSearchContent"
+          <input :value="prodName"
+                 class="sear-input"
+                 confirm-type="search"
+                 placeholder="输入关键字搜索"
+                 @confirm="toSearchConfirm"
+                 @input="getSearchContent"
           >
-          <image
-            class="search-img"
-            src="@/static/images/icon/search.png"
+          <image class="search-img"
+                 src="@/static/images/icon/search.png"
           />
         </view>
-        <view
-          class="search-list-img"
-          @tap="changeShowType"
-        >
-          <image
-            v-if="showType===1"
-            src="@/static/images/icon/search-col.png"
-          />
-          <image
-            v-if="showType===2"
-            src="@/static/images/icon/search-col2.png"
-          />
-        </view>
+
       </view>
+
+      <!-- 搜索排序类型 -->
       <view class="tabs">
         <text
           :class="'tab-item complete ' + (sts===0?'on':'')"
@@ -55,109 +47,81 @@
           价格
         </text>
       </view>
+
     </view>
 
     <!-- 商品列表 -->
     <view class="prod-list">
-      <!-- 横向列表 -->
-      <view
-        v-if="showType===1"
-        class="prod-show"
-      >
-        <view class="hotsale-item-cont">
-          <block
-            v-for="(item, index) in searchProdList"
-            :key="index"
-          >
-            <production
-              :item="item"
-              sts="6"
-            />
-          </block>
-        </view>
-      </view>
 
       <!-- 纵向列表 -->
-      <view
-        v-if="showType===2"
-        class="cont-item"
-      >
+      <view class="cont-item">
+
         <block
           v-for="(item, index) in searchProdList"
           :key="index"
         >
-          <view
-            :data-prodid="item.prodId"
-            class="show-item"
-            @tap="toProdPage"
+          <view :data-prodid="item.prodId"
+                class="show-item"
+                @tap="toProdPage"
           >
             <view class="more-prod-pic">
-              <image
-                :src="item.pic"
-                class="more-pic"
+              <image :src="item.pic"
+                     class="more-pic"
               />
             </view>
+
             <view class="prod-text-right">
+
               <view class="prod-text more">
                 {{ item.prodName }}
               </view>
-              <view class="cate-prod-info">
-                {{ item.praiseNumber }}评价 {{ item.positiveRating }}%好评
-              </view>
+
               <view class="prod-price more">
-                <text class="symbol">
-                  ￥
-                </text>
                 <text class="big-num">
-                  {{ wxs.parsePrice(item.price)[0] }}
-                </text>
-                <text class="small-num">
-                  .{{ wxs.parsePrice(item.price)[1] }}
+                  ￥{{ item.price.toString() }}
                 </text>
               </view>
+
             </view>
+
           </view>
+
         </block>
       </view>
 
-      <!-- 空占位 -->
-      <view
-        v-if="!searchProdList.length"
-        :class="['empty',showType===1? 'empty-top':'']"
-      >
-        暂无结果
-      </view>
+
     </view>
   </view>
 </template>
 
-<script setup>
-import Production from "@/components/production/production.vue";
 
-const wxs = number()
+<script setup>
 const prodName = ref('')
-/**
- * 生命周期函数--监听页面加载
- */
+const sts = ref(0)
+const searchProdList = ref([])
+
+
+//? trigger
+
+
 onLoad((options) => {
   prodName.value = options.prodName
 })
 
-/**
- * 生命周期函数--监听页面显示
- */
+
 onShow(() => {
   toLoadData()
 })
 
-const showType = ref(2)
-const changeShowType = () => {
-  if (showType.value === 1) {
-    showType.value = 2
-  } else {
-    showType.value = 1
-  }
+
+/**
+ * 状态点击事件
+ */
+const onStsTap = (e) => {
+  sts.value = e.currentTarget.dataset.sts
+  toLoadData()
 }
+
 
 /**
  * 输入商品获取数据
@@ -167,10 +131,48 @@ const getSearchContent = (e) => {
   prodName.value = e.detail.value
 }
 
-const sts = ref(0)
-const searchProdList = ref([])
+
+//? 操作
+
+
+//? 页面跳转
+
+
 /**
- *  请求热门搜索商品接口
+ * 前往商品页面
+ * @param e
+ */
+const toProdPage = (e) => {
+  uni.navigateTo({
+    url: '/pages/prod/prod?prodid=' + e.currentTarget.dataset.prodid
+  })
+}
+
+
+/**
+ * 当前搜索页二次搜索
+ */
+const toSearchConfirm = (e) => {
+  if (e.detail.value) {
+    let recentSearch = uni.getStorageSync('recentSearch') || []
+    recentSearch = recentSearch.filter(item => item !== prodName.value)
+    recentSearch.unshift(prodName.value)
+    if (recentSearch.length > 10) {
+      recentSearch.pop()
+    }
+    uni.setStorageSync('recentSearch', recentSearch)
+  }
+  uni.redirectTo({
+    url: '/pages/search-prod-show/search-prod-show?prodName=' + e.detail.value
+  })
+}
+
+
+//? 加载项目
+
+
+/**
+ * 加载数据
  */
 const toLoadData = () => {
   http.request({
@@ -188,37 +190,7 @@ const toLoadData = () => {
     })
 }
 
-/**
- * 当前搜索页二次搜索商品
- */
-const toSearchConfirm = (e) => {
-  if (e.detail.value) {
-    let recentSearch = uni.getStorageSync('recentSearch') || []
-    recentSearch = recentSearch.filter(item => item !== prodName.value)
-    recentSearch.unshift(prodName.value)
-    if (recentSearch.length > 10) {
-      recentSearch.pop()
-    }
-    uni.setStorageSync('recentSearch', recentSearch)
-  }
-  uni.redirectTo({
-    url: '/pages/search-prod-show/search-prod-show?prodName=' + e.detail.value
-  })
-}
 
-/**
- * 状态点击事件
- */
-const onStsTap = (e) => {
-  sts.value = e.currentTarget.dataset.sts
-  toLoadData()
-}
-
-const toProdPage = (e) => {
-  uni.navigateTo({
-    url: '/pages/prod/prod?prodid=' + e.currentTarget.dataset.prodid
-  })
-}
 </script>
 
 <style lang="scss" scoped>
